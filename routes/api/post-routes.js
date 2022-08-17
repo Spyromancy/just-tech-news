94a9a6e02b4f945a2b0a2da0af449fd669565fa7
Josh <josh.haddix598@gmail.com>
2022-08-16 22:23:44 -0500

    added comment functionality

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment} = require('../../models');
 const { route } = require('./user-routes');
 const sequelize = require('../../config/connection');
 
@@ -16,6 +16,14 @@ router.get('/', (req, res) => {
         ],
         order: [['created_at','DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
