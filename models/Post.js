73508e952e642d4d4785f2941a53c52031aa4621
Josh <josh.haddix598@gmail.com>
2022-08-22 21:36:16 -0500

    implemented handlebars

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const { route } = require('../routes');
+const { route } = require('../controllers');
 
 // create Post model
 class Post extends Model {
